2345cd929f7c582a9455650ef2ad469d914b350e
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-28 16:04:20 -0300

    feat: começando a estilizar o footer

---
+++ src/app/Components/Footer/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export function Footer() {
+    return (
+        <footer>
+            <p>
+                © 2020 - Evolução do Saber - por vitukjkkk
+            </p>
+        </footer>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // IMPORTANDO COMPONENTS
 import { Header } from "./Components/Header/page";
+import { Footer } from "./Components/Footer/page";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <h1>Home</h1>
       <p>Welcome to the home page!</p>
+      <Footer />
     </div>
   );
 }
