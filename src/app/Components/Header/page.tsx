77e4a80f5697446a8bb98e68d03864070dbc33c8
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-28 16:22:09 -0300

    feat: estilizando header

--- src/app/Components/Header/page.tsx
+++ src/app/Components/Header/page.tsx
@@ -1,8 +1,14 @@
+import Image from 'next/image';
+import logo_main from '../../../../public/images/logo_saber-evolve.svg';
+import styles from './page.module.css';
 
 export function Header() {
     return (
         <header>
-            <h1>Header</h1>
+            <Image className={styles.imgLogo} src={logo_main} alt="Logo principal"/>
+            <ul>
+                <li>Projetos</li>
+            </ul>
         </header>
     );
 }
