3f42a40a2d8a540b64741727cbd763fb9f46b052
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-28 15:53:01 -0300

    feat: criando header

---
+++ src/app/Components/Header/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <h1>Header</h1>
+        </header>
+    );
+}
