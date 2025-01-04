65fd17ea2313f99f8d9225ccfd67e335b9596330
Vitor Colombelli <vittorcolom@gmail.com>
2025-01-04 16:21:15 -0300

    feat: terminando pagina sobre

--- pages/sobre.tsx
+++ pages/sobre.tsx
@@ -2,8 +2,18 @@ import { Header } from "@/app/Components/Header/page";
 import { Footer } from "@/app/Components/Footer/page";
 
 import Head from "next/head";
+import Image from "next/image";
 import styles from './sobre.module.css';
 import { motion } from "framer-motion";
+
+import logoWhatsApp from '../public/icons/social_media/zap pequeno.png';
+import logoInstagram from '../public/icons/social_media/ig pequeno.png';
+import logoGitHub from '../public/icons/social_media/github pequeno.png';
+import logoFacebook from '../public/icons/social_media/facebook pequeno.png';
+import logoYouTube from '../public/icons/social_media/youtube pequeno.png';
+import logoLinkedIn from '../public/icons/social_media/linkedin pequeno.png';
+import logoPinterest from '../public/icons/social_media/pinterest pequeno.png';
+import logoTikTok from '../public/icons/social_media/tik-tok pequeno.png';
 
 export  default function Sobre() {
     return (
@@ -19,8 +29,58 @@ export  default function Sobre() {
                 animate={{opacity: 1, y: 0}}
                 transition={{duration: 0.5}}>
 
-                <h1>Sobre</h1>
-                <p>Este é um projeto de exemplo para demonstrar o uso do Next.js com TypeScript.</p>
+                <h1> Prazer!! </h1>
+                <p>
+                    Bem-vindo ao meu espaço digital! Me chamo Vitor, e aqui compartilho minha paixão por aprendizado e conhecimento. Sou entusiasta de 
+                    linguagens de programação como JavaScript, C#, TS, Python e SQL, além de ser um praticante de karatê apaixonado pela busca contínua por crescimento pessoal. 
+                    Neste site, você encontrará conteúdos educativos e intuitivos sobre esses temas e muito mais, meus projetos e soluções. 
+                </p>
+
+                <h2>#TMJ</h2>
+        
+                <div className={styles.divSobreContacts}>
+                    <div className={styles.divSobreContactsOne}>
+                        <a href="https://www.linkedin.com/in/vitor-colombelli" target="_blank">
+                            <Image src={logoLinkedIn} alt="Ícone do LinkedIn"/>
+                        </a>
+                    </div>
+                    <div className={styles.divSobreContactsOne}>
+                        <a href="https://wa.me/5561994030749" target="_blank">
+                            <Image src={logoWhatsApp} alt="Ícone do WhatsApp"/>
+                        </a>
+                    </div>
+                    <div className={styles.divSobreContactsOne}>
+                        <a href="https://instagram.com/vitukjkkk" target="_blank">
+                            <Image src={logoInstagram} alt="Ícone do Instagram"/>
+                        </a>
+                    </div>
+                    <div className={styles.divSobreContactsOne}>
+                        <a href="https://www.facebook.com/seituuu.u/" target="_blank">
+                            <Image src={logoFacebook} alt="Ícone do Facebook"/>
+                        </a>
+                    </div>
+                    <div className={styles.divSobreContactsOne}>
+                        <a href="https://github.com/vitukjkk" target="_blank">
+                            <Image src={logoGitHub} alt="Ícone do GitHub"/>
+                        </a>
+                    </div>
+                    <div className={styles.divSobreContactsOne}>
+                        <a href="https://youtube.com/%40vitukjkkk" target="_blank">
+                            <Image src={logoYouTube} alt="Ícone do YouTube"/>
+                        </a>
+                    </div>
+                    <div className={styles.divSobreContactsOne}>
+                        <a href="https://www.tiktok.com/%40vituuuuuu.u?lang=pt-BR" target="_blank">
+                            <Image src={logoTikTok} alt="Ícone do TikTok"/>
+                        </a>
+                    </div>
+                    <div className={styles.divSobreContactsOne}>
+                        <a href="https://br.pinterest.com/vitukjkkk/" target="_blank">
+                            <Image src={logoPinterest} alt="Ícone do Pinterest"/>
+                        </a>
+                    </div>
+                </div>
+
             </motion.div>
             <Footer />
         </div>
