b0e2efaa41261bfb47dbe6d8161b5ad6ff6190c7
Vitor Colombelli <vittorcolom@gmail.com>
2025-01-04 15:28:22 -0300

    feat: terminando projetos de clientes

--- pages/projetos/clientes.tsx
+++ pages/projetos/clientes.tsx
@@ -20,6 +20,42 @@ export default function Clientes() {
                 transition={{duration: 0.5}}>
 
                 <h1>Clientes</h1>
+                <p>
+                    Aqui estão alguns dos projetos que desenvolvi para os meus clientes.
+                </p>
+
+                <div className={styles.divClientesProjects}>
+                    <h1>🍕 Pizzaria e Hamburgueria da May</h1>
+                    <p>
+                        fiz um site em HTML e CSS, com páginas como cardápio, contato, localização e sobre. <br/>Ela gostou muito e eu também (por ser minha primeira experiência), segue o vídeo de apresentação que fiz
+                    </p>
+                    <iframe className={styles.iframeprojectsclients} width="280" height="315" src="https://www.youtube.com/embed/q82zyd60ndc?si=hp-m5b7b0kkqfJK1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen/>
+                    <button>
+                        <a href="https://vitukjkk.github.io/Pizzaria-e-Hamburgueria-da-May/" target="_blank">Clique aqui para acessar o site 🍕</a>
+                    </button>
+                </div>
+
+                <div className={styles.divClientesProjects}>
+                    <h1>🙏 Obrigado por Hoje!</h1>
+                    <p>
+                        este projeto foi idealizado pela minha mãe, que sempre foi bastante católica e me ensinou a agradecer por tudo, então, criei este site para as pessoas exercitarem sua gratidão diária. Segue o vídeo de apresentação que fiz
+                    </p>
+                    <iframe className={styles.iframeprojectsclients} width="280" height="315" src="https://www.youtube.com/embed/P3D_S5LH0N4?si=ojTsHehNLwIHB2G6" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen/>
+                    <button>
+                        <a href="https://vitukjkk.github.io/ObrigadoPorHoje/" target="_blank">Clique aqui para acessar o site 🙏</a>
+                    </button>
+                </div>
+
+                <div className={styles.divClientesProjects}>
+                    <h1>🎥 Papos & Retratos</h1>
+                    <p>
+                        este é um projeto educacional do IFB, onde aplicamos todas as etapas de um projeto real, desde assinatura de contrato até elaboração de wireframe. Fizemos um site para um fotógrafo fictício, com páginas como portfólio, contato, sobre e blog.
+                    </p>
+                    <button className={styles.buttonprojectsclients}>
+                        Sem site disponível por enquanto :(
+                    </button>
+                    <i>por enquanto estamos em fase de implementação do código, por isso não posso compartilhar o código, mas em breve estará aqui o vídeo de introdução!</i>
+                </div>
             </motion.div>
             <Footer/>
         </div>
