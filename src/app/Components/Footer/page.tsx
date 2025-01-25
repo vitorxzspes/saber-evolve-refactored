61a51da351cf44c2a84adb3de7491be168f89176
Vitor Colombelli <vittorcolom@gmail.com>
2025-01-25 10:40:33 -0300

    feat: atualizando username github no footer

--- src/app/Components/Footer/page.tsx
+++ src/app/Components/Footer/page.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
     return (
         <footer>
             <p>
-                © 2024 - Evolução do Saber - por <a href="https://github.com/vitukjkk/" target="_blank" rel="noopener noreferrer"
-                >vitukjkkk</a>
+                © 2024 - Evolução do Saber - por <a href="https://github.com/vitorxzspes/" target="_blank" rel="noopener noreferrer"
+                >vitorxzspes</a>
             </p>
         </footer>
     );
